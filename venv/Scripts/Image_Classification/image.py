6e4208469d619209389a48b42bb1513d8091a857
Karthiga <karthiga.easwar16@gmail.com>
2021-03-12 11:28:17 +0000

    downloading the image dataset

---
+++ venv/Scripts/Image_Classification/image.py
@@ -0,0 +1,10 @@
+import keras
+import tensorflow
+from keras.datasets import cifar10
+import matplotlib.pyplot as plt
+
+#Loading the dataset
+(train_X, train_Y), (test_X, test_Y) = cifar10.load_data()
+
+
+
