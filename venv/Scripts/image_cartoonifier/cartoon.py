d8732cf437326904dd0504884c5d6d38cbfca083
Karthiga <karthiga.easwar16@gmail.com>
2021-05-13 11:25:28 +0100

    cartoon

---
+++ venv/Scripts/image_cartoonifier/cartoon.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import cv2 #for image processing
+import sys
+import easygui
+import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import *
+from PIL import ImageTk , Image
+import imageio # to read image stored at particular path
+import os
+
+def uploadfile()
+
+
